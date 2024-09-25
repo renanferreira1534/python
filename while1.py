235b3e9503e8d3049c58261253cb5f6cef0e2b71
renan ferreira <renanferreira1534@gmail.com>
2024-09-25 09:25:41 -0300

    Trabalhando com while

---
+++ while1.py
@@ -0,0 +1,6 @@
+# fazeruma contagem de 1 a 10
+i = 1
+
+while i <= 10:
+    print("contei ->"+ str(i))
+    i+=1
---
+++ while2.py
@@ -0,0 +1,6 @@
+num = 0
+while num <= 100:
+    if num % 2 == 0:
+        print(f"Este numero é par {num}")
+    num+=1
+
